c7d9be563148150228acee39e53c744d800f02a7
Dev <96375187+Dev-raj01@users.noreply.github.com>
2025-02-21 15:21:04 +0530

    top k elements medium heap and bucket sort

---
+++ top-k-frequent-elements_heap_bucketSORT.cpp
@@ -0,0 +1,59 @@
+//https://leetcode.com/problems/top-k-frequent-elements/description/
+
+//done with heap and bucket short
+// O(n) time and O(n) space with bucket sort
+// O(nlogk) time and O(n) space with heap
+
+//heap
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> count;
+        for (int num : nums) {
+            count[num]++;
+        }
+
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> heap;
+        for (auto& entry : count) {
+            heap.push({entry.second, entry.first});
+            if (heap.size() > k) {
+                heap.pop();
+            }
+        }
+
+        vector<int> res;
+        for (int i = 0; i < k; i++) {
+            res.push_back(heap.top().second);
+            heap.pop();
+        }
+        return res;
+    }
+};
+
+
+//bucket sort
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> count;
+        vector<vector<int>> freq(nums.size() + 1);
+
+        for (int n : nums) {
+            count[n] = 1 + count[n];
+        }
+        for (const auto& entry : count) {
+            freq[entry.second].push_back(entry.first);
+        }
+
+        vector<int> res;
+        for (int i = freq.size() - 1; i > 0; --i) {
+            for (int n : freq[i]) {
+                res.push_back(n);
+                if (res.size() == k) {
+                    return res;
+                }
+            }
+        }
+        return res;
+    }
+};
