64874db1d1580dfc6550c01c2fc06b05f23efcd3
Dev <96375187+Dev-raj01@users.noreply.github.com>
2025-02-22 10:11:59 +0530

    3some and container with most water done with 2 pointer approach

--- container-with-most-water.cpp
+++ container-with-most-water.cpp
@@ -15,3 +15,22 @@ public:
        return(ans);
     }
 }
+
+//better with 2pointer
+//O(n) time and O(1) space
+
+class Solution {
+public:
+    int maxArea(vector<int>& h) {
+        int l=0,r=h.size()-1;
+        int ans=min(h[l],h[r])*(r-l);
+        while(l<r){
+            if(h[l]<h[r])
+                l++;
+            else 
+                r--;
+            ans=max(ans,(min(h[l],h[r])*(r-l)));
+        }
+        return ans;
+    }
+};
