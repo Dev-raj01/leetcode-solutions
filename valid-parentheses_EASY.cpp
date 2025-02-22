fd382bfc1ca58830a1c23e3066c8fb6d3c27d695
Dev <96375187+Dev-raj01@users.noreply.github.com>
2025-02-22 19:06:56 +0530

    valid parentheses easy hash

---
+++ valid-parentheses_EASY.cpp
@@ -0,0 +1,27 @@
+//https://leetcode.com/problems/valid-parentheses/?source=submission-ac
+//easy stack
+
+class Solution {
+public:
+    bool isValid(string s) {
+        std::stack<char> stack;
+        std::unordered_map<char, char> closeToOpen = {
+            {')', '('},
+            {']', '['},
+            {'}', '{'}
+        };
+
+        for (char c : s) {
+            if (closeToOpen.count(c)) {
+                if (!stack.empty() && stack.top() == closeToOpen[c]) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.empty();
+    }
+};
