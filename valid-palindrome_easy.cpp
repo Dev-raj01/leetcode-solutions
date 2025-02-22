57b5db86ae477736fb1e0b637ff2bf10d329f903
Dev <96375187+Dev-raj01@users.noreply.github.com>
2025-02-22 09:06:44 +0530

    valid palindrome easy

---
+++ valid-palindrome_easy.cpp
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/valid-palindrome/submissions/1551301561/
+
+//easy question
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int l=0,r=s.length()-1;
+        while(l<r){
+            while(l<r && !isalnum(s[l]))
+                l++;
+            while(l<r && !isalnum(s[r]))
+                r--;
+            if(tolower(s[l])!=tolower(s[r]))
+                return false;
+            l++,r--;
+        }
+        return true;          
+    }
+};
